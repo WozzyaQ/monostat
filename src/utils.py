9362f4a54440467f24a0638a448767245a7bb74b
Vladyslav <chaikovskyivv@gmail.com>
2022-05-04 23:04:31 +0300

    refactor: remove flask sources

--- src/utils.py
+++
@@ -1,4 +0,0 @@
-def map_from_dict_like(model, dict_like: dict):
-    attributes = [attr for attr in vars(model) if not attr.startswith("_")]
-    model_attributes = {key: dict_like[key] for key in attributes}
-    return model(**model_attributes)
