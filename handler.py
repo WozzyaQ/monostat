c7c5a654407e2a1ea6e945f34985c0d0b9f61792
Vladyslav <chaikovskyivv@gmail.com>
2022-05-04 23:06:57 +0300

    chore: add dummy handler module

---
+++ handler.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
